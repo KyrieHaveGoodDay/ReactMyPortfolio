8a02bb92f8a1c8d492c471dd70502389081842a2
kyriehavegoodday <healthymbear@gmail.com>
2023-10-24 23:43:28 +0800

    路徑修改

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,14 @@
+import {useNavigate } from 'react-router-dom';
+
 function Footer(){
     const today = new Date();
     const todayYear = today.getFullYear();
+    const navigate = useNavigate();
+
     return<>
         <footer>
             <div id="icon">
-                <a href="../index.html">
+                <a onClick={()=>{navigate('/')}} style={{cursor:'pointer'}}>
                     <h3>KyrieHaveGoodDay</h3>
                 </a>
                 <p>Copyright &copy;<span>{todayYear}</span> KyrieHaveGoodDay</p>
